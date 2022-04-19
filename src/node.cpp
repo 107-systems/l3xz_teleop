18d92fe81ae2e7a38353cee2eaeb7d91111a06bb
Alexander Entinger <cto@lxrobotics.com>
2022-04-19 08:47:18 +0200

    Publish a geometry_msgs::Twist with a update rate of 20 Hz (50 ms).

--- src/node.cpp
+++ src/node.cpp
@@ -9,10 +9,12 @@
  **************************************************************************************/
 
 #include <string>
-#include <thread>
+#include <chrono>
 
 #include <ros/ros.h>
 #include <ros/console.h>
+
+#include <geometry_msgs/Twist.h>
 
 #include <l3xz_teleop/Joystick.h>
 
@@ -20,40 +22,68 @@
  * MAIN
  **************************************************************************************/
 
-int main(int argc, char **argv) try
+int main(int argc, char **argv)
 {
   ros::init(argc, argv, "teleop_node");
 
   ros::NodeHandle node_hdl;
   ros::NodeHandle node_hdl_private("~");
 
-  std::string joy_dev_node;
+  std::string joy_dev_node, joy_topic;
 
   node_hdl_private.param<std::string>("joy_dev_node", joy_dev_node, "/dev/input/js0");
+  node_hdl_private.param<std::string>("joy_topic", joy_topic, "cmd_vel");
 
-  ROS_INFO("node config:\n  joy_dev_node: %s", joy_dev_node.c_str());
+  ROS_INFO("node config:\n  joy_dev_node: %s\n  joy_topic   : %s", joy_dev_node.c_str(), joy_topic.c_str());
 
-  Joystick joystick(joy_dev_node);
+  ros::Publisher cmd_vel_pub = node_hdl.advertise<geometry_msgs::Twist>(joy_topic, 10);
 
-  while (ros::ok())
+  try
   {
-    std::optional<JoystickEvent> const evt = joystick.update();
+    Joystick joystick(joy_dev_node);
 
-    if (evt)
+    for (auto prev = std::chrono::steady_clock::now(); ros::ok(); )
     {
-      if (evt.value().isAxis())
-      {
-        ROS_INFO("Axis %d: %d", evt.value().number, evt.value().value);
-      }
-    }
+      /* Read data from the joystick and pre-process it. */
+      std::optional<JoystickEvent> const evt = joystick.update();
 
-    ros::spinOnce();
+      if (evt)
+      {
+        if (evt.value().isAxis())
+        {
+          ROS_INFO("Axis %d: %d", evt.value().number, evt.value().value);
+        }
+      }
+
+      /* Transmit control messages at a defined interval. */
+      auto const now = std::chrono::steady_clock::now();
+      auto const elapsed = (now - prev);
+
+      if (elapsed > std::chrono::milliseconds(50))
+      {
+        prev = now;
+
+        geometry_msgs::Twist msg;
+
+        msg.linear.x  =  1.0;
+        msg.linear.y  = -1.0;
+        msg.linear.z  =  0.0;
+
+        msg.angular.x =  1.0;
+        msg.angular.y = -1.0;
+        msg.angular.z =  0.0;
+
+        cmd_vel_pub.publish(msg);
+      }
+
+      ros::spinOnce();
+    }
+  }
+  catch (std::runtime_error const & err)
+  {
+    ROS_ERROR("%s", err.what());
+    return EXIT_FAILURE;
   }
 
   return EXIT_SUCCESS;
-}
-catch (std::runtime_error const & err)
-{
-  ROS_ERROR("%s", err.what());
-  return EXIT_FAILURE;
 }
