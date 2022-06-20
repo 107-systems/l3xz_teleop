85d307c69924ec0cd0405273d29aeca80f8152a2
Alexander Entinger <cto@lxrobotics.com>
2022-06-20 09:14:18 +0200

    Split reading of joystick and publishing for reduced control-flow-complexity.

--- include/l3xz_teleop/TeleopNode.h
+++ include/l3xz_teleop/TeleopNode.h
@@ -8,12 +8,14 @@
  * INCLUDE
  **************************************************************************************/
 
+#include <map>
 #include <memory>
 
 #include <rclcpp/rclcpp.hpp>
 #include <geometry_msgs/msg/twist.hpp>
 
 #include "Joystick.h"
+#include "PS3_Const.h"
 
 /**************************************************************************************
  * CLASS DECLARATION
@@ -23,11 +25,15 @@ class TeleopNode : public rclcpp::Node
 {
 public:
   TeleopNode();
-  void update();
 
+  void update();
 
 private:
   rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr _publisher;
   std::shared_ptr<Joystick> _joystick;
   geometry_msgs::msg::Twist _twist_msg;
+  std::map<PS3_AxisId, float> _axis_data;
+
+  void update_joystick();
+  void update_ros();
 };
--- src/TeleopNode.cpp
+++ src/TeleopNode.cpp
@@ -10,12 +10,9 @@
 
 #include <l3xz_teleop/TeleopNode.h>
 
-#include <map>
 #include <chrono>
 #include <limits>
 #include <numeric>
-
-#include <l3xz_teleop/PS3_Const.h>
 
 /**************************************************************************************
  * CTOR/DTOR 
@@ -49,70 +46,77 @@ TeleopNode::TeleopNode()
 
 void TeleopNode::update()
 {
-  static bool initialized = false;
-  static auto prev = std::chrono::steady_clock::now();
+  update_joystick();
 
+  static auto prev = std::chrono::steady_clock::now();
+  auto const now = std::chrono::steady_clock::now();
+
+  if ((now - prev) > std::chrono::milliseconds(50)) {
+    prev = now;
+    update_ros();
+  }
+}
+
+/**************************************************************************************
+ * PRIVATE MEMBER FUNCTIONS
+ **************************************************************************************/
+
+void TeleopNode::update_joystick()
+{
   std::optional<JoystickEvent> const evt = _joystick->update();
 
-  if (evt)
+  if (!evt.has_value())
+    return;
+
+  if (evt.value().isInit())
+    return;
+
+  if (evt.value().isAxis())
   {
-    std::map<PS3_AxisId, float> axis_data;
+    RCLCPP_INFO(get_logger(), "Axis %d: %d", evt.value().number, evt.value().value);
 
-    if (!initialized)
-    {
-      initialized = evt.value().isInit();
-    }
-    else if (evt.value().isAxis())
-    {
-      RCLCPP_INFO(get_logger(), "Axis %d: %d", evt.value().number, evt.value().value);
-
-      PS3_AxisId const axis_id = static_cast<PS3_AxisId>(evt.value().number);
-      float const axis_scaled_val = static_cast<float>(evt.value().value) / static_cast<float>(std::numeric_limits<int16_t>::max());
-      axis_data[axis_id] = axis_scaled_val;
-    }
-
-    if (evt.value().isButton())
-    {
-      RCLCPP_INFO(get_logger(), "Button %d: %d", evt.value().number, evt.value().value);
-    }
-
-    auto const now = std::chrono::steady_clock::now();
-    auto const elapsed = (now - prev);
-
-    if (elapsed > std::chrono::milliseconds(50))
-    {
-      prev = now;
-      float linear_velocity_x = _twist_msg.linear.x;
-      if (axis_data.count(PS3_AxisId::LEFT_STICK_VERTICAL))
-        linear_velocity_x = -1.0f * axis_data[PS3_AxisId::LEFT_STICK_VERTICAL];
-
-      float linear_velocity_y = _twist_msg.linear.y;
-      if (axis_data.count(PS3_AxisId::LEFT_STICK_HORIZONTAL))
-        linear_velocity_y = axis_data[PS3_AxisId::LEFT_STICK_HORIZONTAL];
-
-      float angular_velocity_head_tilt = _twist_msg.angular.x;
-      if (axis_data.count(PS3_AxisId::RIGHT_STICK_VERTICAL))
-        angular_velocity_head_tilt = -1.0f * axis_data[PS3_AxisId::RIGHT_STICK_VERTICAL];
-
-      float angular_velocity_head_pan = _twist_msg.angular.y;
-      if (axis_data.count(PS3_AxisId::RIGHT_STICK_HORIZONTAL))
-        angular_velocity_head_pan = axis_data[PS3_AxisId::RIGHT_STICK_HORIZONTAL];
-
-      float angular_velocity_z = 0.0f;
-      if (axis_data.count(PS3_AxisId::LEFT_REAR_2))
-        angular_velocity_z -= (axis_data[PS3_AxisId::LEFT_REAR_2] + 1.0f) / 2.0f;
-      if (axis_data.count(PS3_AxisId::RIGHT_REAR_2))
-        angular_velocity_z += (axis_data[PS3_AxisId::RIGHT_REAR_2] + 1.0f) / 2.0f;
-
-      _twist_msg.linear.x  = linear_velocity_x;
-      _twist_msg.linear.y  = linear_velocity_y;
-      _twist_msg.linear.z  = 0.0;
-
-      _twist_msg.angular.x = angular_velocity_head_tilt;
-      _twist_msg.angular.y = angular_velocity_head_pan;
-      _twist_msg.angular.z = angular_velocity_z;
-
-      _publisher->publish(_twist_msg);
-    }
+    PS3_AxisId const axis_id = static_cast<PS3_AxisId>(evt.value().number);
+    float const axis_scaled_val = static_cast<float>(evt.value().value) / static_cast<float>(std::numeric_limits<int16_t>::max());
+    _axis_data[axis_id] = axis_scaled_val;
   }
+
+  if (evt.value().isButton())
+  {
+    RCLCPP_INFO(get_logger(), "Button %d: %d", evt.value().number, evt.value().value);
+  }
+}
+
+void TeleopNode::update_ros()
+{
+  float linear_velocity_x = _twist_msg.linear.x;
+  if (_axis_data.count(PS3_AxisId::LEFT_STICK_VERTICAL))
+    linear_velocity_x = -1.0f * _axis_data[PS3_AxisId::LEFT_STICK_VERTICAL];
+
+  float linear_velocity_y = _twist_msg.linear.y;
+  if (_axis_data.count(PS3_AxisId::LEFT_STICK_HORIZONTAL))
+    linear_velocity_y = _axis_data[PS3_AxisId::LEFT_STICK_HORIZONTAL];
+
+  float angular_velocity_head_tilt = _twist_msg.angular.x;
+  if (_axis_data.count(PS3_AxisId::RIGHT_STICK_VERTICAL))
+    angular_velocity_head_tilt = -1.0f * _axis_data[PS3_AxisId::RIGHT_STICK_VERTICAL];
+
+  float angular_velocity_head_pan = _twist_msg.angular.y;
+  if (_axis_data.count(PS3_AxisId::RIGHT_STICK_HORIZONTAL))
+    angular_velocity_head_pan = _axis_data[PS3_AxisId::RIGHT_STICK_HORIZONTAL];
+
+  float angular_velocity_z = 0.0f;
+  if (_axis_data.count(PS3_AxisId::LEFT_REAR_2))
+    angular_velocity_z -= (_axis_data[PS3_AxisId::LEFT_REAR_2] + 1.0f) / 2.0f;
+  if (_axis_data.count(PS3_AxisId::RIGHT_REAR_2))
+    angular_velocity_z += (_axis_data[PS3_AxisId::RIGHT_REAR_2] + 1.0f) / 2.0f;
+
+  _twist_msg.linear.x  = linear_velocity_x;
+  _twist_msg.linear.y  = linear_velocity_y;
+  _twist_msg.linear.z  = 0.0;
+
+  _twist_msg.angular.x = angular_velocity_head_tilt;
+  _twist_msg.angular.y = angular_velocity_head_pan;
+  _twist_msg.angular.z = angular_velocity_z;
+
+  _publisher->publish(_twist_msg);
 }
