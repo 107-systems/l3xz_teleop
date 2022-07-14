b7f9579744b0a37cfa85f96c20443482f7a32644
Alexander Entinger <cto@lxrobotics.com>
2022-07-14 05:54:38 +0200

    Completely cutting out all joystick driver related code.

--- include/l3xz_teleop/TeleopNode.h
+++ include/l3xz_teleop/TeleopNode.h
@@ -8,17 +8,9 @@
  * INCLUDE
  **************************************************************************************/
 
-#include <map>
-#include <mutex>
-#include <memory>
-#include <thread>
-
 #include <rclcpp/rclcpp.hpp>
 
 #include <geometry_msgs/msg/twist.hpp>
-
-#include "Joystick.h"
-#include "PS3_Const.h"
 
 /**************************************************************************************
  * CLASS DECLARATION
@@ -27,8 +19,7 @@
 class TeleopNode : public rclcpp::Node
 {
 public:
-   TeleopNode();
-  ~TeleopNode();
+  TeleopNode();
 
 private:
   rclcpp::TimerBase::SharedPtr _teleop_pub_timer;
@@ -38,13 +29,5 @@ private:
   rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr _teleop_pad_pub;
   geometry_msgs::msg::Twist _msg_pad;
 
-  std::shared_ptr<Joystick> _joystick;
-  std::map<PS3_AxisId, float> _joystick_data;
-  std::map<PS3_ButtonId, bool> _button_data;
-  std::mutex _joy_mtx;
-  std::thread _joy_thread;
-  std::atomic<bool> _joy_thread_active;
-
-  void joystickThreadFunc();
   void teleopTimerCallback();
 };
--- launch/joy.py
+++ launch/joy.py
@@ -13,12 +13,6 @@ def generate_launch_description():
           {'joy_dev_node': '/dev/input/js0'},
           {'topic_robot_stick': 'cmd_vel_robot'},
           {'topic_robot_pad': 'cmd_vel_head'},
-          {'robot_linear_x_scale_factor': 1.0},
-          {'robot_linear_y_scale_factor': 1.0},
-          {'robot_angular_x_scale_factor': 1.0},
-          {'robot_angular_z_scale_factor': 1.0},
-          {'head_angular_y_scale_factor': 1.0},
-          {'head_angular_z_scale_factor': 1.0},
       ]
     )
   ])
